4feba2f48c2dd28530bb2cbac307e6cabf0681ae
rickmurdock <rick.murdock@live.com>
2017-06-15 09:25:07 -0500

    Added conversion tor state codes

--- main.js
+++ main.js
@@ -45,6 +45,10 @@
     custName.innerHTML=  (cust[index].name.first + " " + cust[index].name.last).toUpperCase();
     custDiv.appendChild(custName);
 
+    var line = document.createElement('div');
+    line.className = "line";
+    custDiv.appendChild(line);
+
     var custEmail = document.createElement('p');
     custEmail.className = 'email';
     custEmail.innerHTML = (cust[index].email).toUpperCase();
@@ -53,12 +57,12 @@
     var custAddr = document.createElement('p');
     custAddr.innerHTML = toTitleCase(cust[index].location.street) + '<br>' +
                         toTitleCase(cust[index].location.city) + ", " + 
-                        toTitleCase(cust[index].location.state) + " " +
+                        getStateCode(toTitleCase(cust[index].location.state)) + " " +
                         cust[index].location.postcode;
     custDiv.appendChild(custAddr);
 
     var custPhone = document.createElement('p');
-    custPhone.innerHTML = cust[index].cell;
+    custPhone.innerHTML = (cust[index].cell).replace('(', '').replace(')', '');
     custDiv.appendChild(custPhone);
 
     var custSSN= document.createElement('p');
@@ -72,6 +76,74 @@
       return str.replace(/\w\S*/g, function(txt){
           return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
         });
+  }
+
+  function getStateCode(state) {
+
+    var  states_hash =
+    {
+      'Alabama': 'AL',
+      'Alaska': 'AK',
+      'American Samoa': 'AS',
+      'Arizona': 'AZ',
+      'Arkansas': 'AR',
+      'California': 'CA',
+      'Colorado': 'CO',
+      'Connecticut': 'CT',
+      'Delaware': 'DE',
+      'District Of Columbia': 'DC',
+      'Federated States Of Micronesia': 'FM',
+      'Florida': 'FL',
+      'Georgia': 'GA',
+      'Guam': 'GU',
+      'Hawaii': 'HI',
+      'Idaho': 'ID',
+      'Illinois': 'IL',
+      'Indiana': 'IN',
+      'Iowa': 'IA',
+      'Kansas': 'KS',
+      'Kentucky': 'KY',
+      'Louisiana': 'LA',
+      'Maine': 'ME',
+      'Marshall Islands': 'MH',
+      'Maryland': 'MD',
+      'Massachusetts': 'MA',
+      'Michigan': 'MI',
+      'Minnesota': 'MN',
+      'Mississippi': 'MS',
+      'Missouri': 'MO',
+      'Montana': 'MT',
+      'Nebraska': 'NE',
+      'Nevada': 'NV',
+      'New Hampshire': 'NH',
+      'New Jersey': 'NJ',
+      'New Mexico': 'NM',
+      'New York': 'NY',
+      'North Carolina': 'NC',
+      'North Dakota': 'ND',
+      'Northern Mariana Islands': 'MP',
+      'Ohio': 'OH',
+      'Oklahoma': 'OK',
+      'Oregon': 'OR',
+      'Palau': 'PW',
+      'Pennsylvania': 'PA',
+      'Puerto Rico': 'PR',
+      'Rhode Island': 'RI',
+      'South Carolina': 'SC',
+      'South Dakota': 'SD',
+      'Tennessee': 'TN',
+      'Texas': 'TX',
+      'Utah': 'UT',
+      'Vermont': 'VT',
+      'Virgin Islands': 'VI',
+      'Virginia': 'VA',
+      'Washington': 'WA',
+      'West Virginia': 'WV',
+      'Wisconsin': 'WI',
+      'Wyoming': 'WY'
+    }
+
+    return states_hash[state];
   }
  
 })();
